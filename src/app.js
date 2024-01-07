fda2f1c7d05828f2eb63dd43be0158d832ffc8a6
JessePomeroy <thinkingofview@gmail.com>
2024-01-07 11:34:31 -0500

    fixed a little routing error with the home page

--- src/app.js
+++ src/app.js
@@ -1,52 +1,69 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AboutMe from "./components/about";
 import BlackRabbit from "./components/blackrabbit";
 import { Carousel } from "./components/carousel/carousel";
 import Contact from "./components/contact/contact";
 import Footer from "./components/footer";
-import Header from "./components/header";
-
 import Navbar from "./components/navbar";
-import "./style/index.css";
 
-function App() {
-    const [navVisible, showNavbar] = useState(false);
+export default function App() {
+  const [navVisible, showNavbar] = useState(false);
 
-    return (
-        <BrowserRouter >
-            <div className="App">
-                <Navbar visible={navVisible} show={showNavbar} />
-                <Routes>
-                    <Route path="/react-port" element={<Navigate to="/about" />} />
-                    <Route path='/projects' element={
-                        <div className={!navVisible ? "page" : "page page-with-navbar"}>
-                            <Carousel />
-                            <Footer />
-                        </div>
-                    } />
-                    <Route path='/blackrabbit' element={
-                        <div className={!navVisible ? "page" : "page page-with-navbar"}>
-                            <BlackRabbit />
-                            <Footer />
-                        </div>
-                    } />
-                    <Route path='/contact' element={
-                        <div className={!navVisible ? "page" : "page page-with-navbar"}>
-                            <Contact />
-                            <div className="carousel-item-text">working on creating a modal on click with form submit to enter name and email with an autoresponder</div>
-                            <Footer />
-                        </div>
-                    } />
-                    <Route path='/about' element={
-                        <div className={!navVisible ? "page" : "page page-with-navbar"}>
-                            <AboutMe />
-                        </div>
-                    } />
-                </Routes>
-            </div >
-        </BrowserRouter >
-    );
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar visible={navVisible} show={showNavbar} />
+        <Routes>
+          <Route
+            path="/react-port"
+            element={
+              <div className={!navVisible ? "page" : "page page-with-navbar"}>
+                <AboutMe />
+              </div>
+            }
+          />
+          <Route
+            path="/projects"
+            element={
+              <div className={!navVisible ? "page" : "page page-with-navbar"}>
+                <Carousel />
+                <Footer />
+              </div>
+            }
+          />
+          <Route
+            path="/blackrabbit"
+            element={
+              <div className={!navVisible ? "page" : "page page-with-navbar"}>
+                <BlackRabbit />
+                <Footer />
+              </div>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <div className={!navVisible ? "page" : "page page-with-navbar"}>
+                <Contact />
+                <div className="carousel-item-text">
+                  working on creating a modal on click with form submit to enter
+                  name and email with an autoresponder
+                </div>
+                <Footer />
+              </div>
+            }
+          />
+          {/* <Route
+            path="/react-port"
+            element={
+              <div className={!navVisible ? "page" : "page page-with-navbar"}>
+                <AboutMe />
+              </div>
+            }
+          /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
-
-export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,14 @@
 import {
-    FaAngleLeft,
-    FaAngleRight,
-    FaBars,
-    FaGithubAlt,
-    FaHome,
-    FaReadme,
-    FaRegUser,
-    FaTerminal,
-    FaTree
-} from 'react-icons/fa';
+  FaAngleLeft,
+  FaAngleRight,
+  FaBars,
+  FaGithubAlt,
+  FaHome,
+  FaReadme,
+  FaRegUser,
+  FaTerminal,
+  FaTree,
+} from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import "../style/navbar.css";
 // import { Container } from './container';
@@ -25,69 +25,57 @@ const ICON_SIZE = 20;
 // eventually plan to implement this modal and have it work with an auto-responder
 
 function Navbar({ visible, show }) {
+  return (
+    <>
+      <div className="mobile-nav">
+        <button className="mobile-nav-btn" onClick={() => show(!visible)}>
+          <FaBars size={24} />
+        </button>
+      </div>
+      <nav className={!visible ? "navbar" : ""}>
+        <button
+          type="button"
+          className="nav-btn"
+          onClick={() => show(!visible)}
+        >
+          {!visible ? <FaAngleRight size={30} /> : <FaAngleLeft size={30} />}
+        </button>
+        <div>
+          <NavLink className="logo" to="https://github.com/jessepomeroy">
+            <img src={require("../assets/images/logo.png")} alt="logo" />
+          </NavLink>
+          <div className="links nav-top">
+            <NavLink to="/projects" className="nav-link">
+              <FaReadme size={ICON_SIZE} />
+              <span>Projects</span>
+            </NavLink>
+            <NavLink to="/blackrabbit" className="nav-link">
+              <FaTree size={ICON_SIZE} />
+              <span>Black Rabbit</span>
+            </NavLink>
+          </div>
+        </div>
 
-    return (
-        <>
-            <div className="mobile-nav">
-                <button
-                    className="mobile-nav-btn"
-                    onClick={() => show(!visible)}
-                >
-                    <FaBars size={24} />
-                </button>
-            </div>
-            <nav className={!visible ? 'navbar' : ''}>
-                <button
-                    type="button"
-                    className="nav-btn"
-                    onClick={() => show(!visible)}
-                >
-                    {!visible
-                        ? <FaAngleRight size={30} /> : <FaAngleLeft size={30} />}
-                </button>
-                <div>
-                    <NavLink
-                        className="logo"
-                        to="https://github.com/jessepomeroy"
-                    >
-                        <img
-                            src={require("../assets/images/logo.png")}
-                            alt="logo"
-                        />
-                    </NavLink>
-                    <div className="links nav-top">
-                        <NavLink to="/projects" className="nav-link">
-                            <FaReadme size={ICON_SIZE} />
-                            <span>Projects</span>
-                        </NavLink>
-                        <NavLink to="/blackrabbit" className="nav-link">
-                            <FaTree size={ICON_SIZE} />
-                            <span>Black Rabbit</span>
-                        </NavLink>
-
-                    </div>
-                </div>
-
-                <div className="links">
-                    <NavLink to="/about" className="nav-link">
-                        <FaHome size={ICON_SIZE} />
-                        <span>Home</span>
-                    </NavLink>
-                    <NavLink to="https://github.com/jessepomeroy" className="nav-link">
-                        <FaGithubAlt size={ICON_SIZE} />
-                        <span>Github</span>
-                    </NavLink>
-                    <div>
-                        <NavLink to="/contact" className="nav-link">
-                            <FaTerminal size={ICON_SIZE} />
-                            <span>Contact</span>
-                            {/* <Container triggerText={triggerText} onSubmit={onSubmit} /> */}
-                        </NavLink>
-                    </div>
-                </div>
-            </nav >
-        </>
-    );
+        <div className="links">
+          <NavLink to="/react-port" className="nav-link">
+            <FaHome size={ICON_SIZE} />
+            <span>Home</span>
+          </NavLink>
+          <NavLink to="https://github.com/jessepomeroy" className="nav-link">
+            <FaGithubAlt size={ICON_SIZE} />
+            <span>Github</span>
+          </NavLink>
+          <div>
+            <NavLink to="/contact" className="nav-link">
+              <FaTerminal size={ICON_SIZE} />
+              <span>Contact</span>
+              {/* <Container triggerText={triggerText} onSubmit={onSubmit} /> */}
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
